204ce8bfec49b23f76f3e59e6a4b09ddcc0963fa
Jianend <2110179446@qq.com>
2022-10-04 19:01:07 +0800

    Initial commit

---
+++ src/main/java/sql.java
@@ -0,0 +1,10 @@
+import com.mysql.jdbc.Driver;
+
+import java.sql.SQLException;
+
+public class sql {
+    public static void main(String[] args)  throws SQLException {
+        Driver driver = new Driver();
+        String url ="jdbc://localhost:3306/lanqiao";
+    }
+}
